24d53498cb4c65a61852fe148109e35a15da3870
Diego-Hinrichs <diegooffaa@outlook.com>
2023-11-14 20:49:12 -0300

    este si funciona

---
+++ utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+places = np.array(['Cervecería', 'Parque', 'Plaza', 'Población', 
+                   'Villa', 'Pasaje', 'Sector', 'Hospital', 'Escuela', 
+                   'Colegio', 'Universidad', 'Cine', 'Puente', 
+                   'Avenida', 'Calle', 'Barrio', 'Estación', 'Mercado', 
+                   'Biblioteca', 'Centro comercial', 'Terminal', 
+                   'Jardín', 'Museo', 'Teatro', 'Galería', 'Estadio', 
+                   'Café', 'Restaurante', 'Iglesia', 'Oficina', 
+                   'Embajada', 'Estación de policía', 'Estación de bomberos', 
+                   'Estación de tren', 'Estación de autobuses', 'Aeropuerto', 
+                   'Puerto', 'Playa', 'Montaña', 'Río', 'Lago', 'Bosque', 
+                   'Zoológico', 'Acuario', 'Mirador', 'Monumento', 'Torre', 
+                   'Faro', 'Camino', 'Sendero'])
+
+
+print(places)
