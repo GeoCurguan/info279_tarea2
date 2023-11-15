f9912a8006f64beec58cdf830c70e711045b4ce1
Diego-Hinrichs <diegooffaa@outlook.com>
2023-11-14 22:22:08 -0300

    api

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import requests
 
 places = np.array(['Cervecería', 'Parque', 'Plaza', 'Población', 
                    'Villa', 'Pasaje', 'Sector', 'Hospital', 'Escuela', 
@@ -10,8 +11,19 @@ places = np.array(['Cervecería', 'Parque', 'Plaza', 'Población',
                    'Embajada', 'Estación de policía', 'Estación de bomberos', 
                    'Estación de tren', 'Estación de autobuses', 'Aeropuerto', 
                    'Puerto', 'Playa', 'Montaña', 'Río', 'Lago', 'Bosque', 
-                   'Zoológico', 'Acuario', 'Mirador', 'Monumento', 'Torre', 
+                   'Acuario', 'Mirador', 'Monumento', 'Torre', 
                    'Faro', 'Camino', 'Sendero'])
 
 
-print(places)
+def query(texto: str):
+    url = 'https://places.googleapis.com/v1/places:searchText'
+    data = {
+        'textQuery': texto
+    }
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': 'AIzaSyAjbtkLeCtZfQWp5zOyIGwmrBSpCtq9Lps',
+        'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.priceLevel'
+    }
+    response = requests.post(url, json=data, headers=headers)
+    return response
